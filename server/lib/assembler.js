5002c83ab94d3ae42d5164b18a4f43304b6931bb
Chris Hegre <christopher.hegre@bigcommerce.com>
2015-04-14 14:35:51 -0700

    Added option renderables and context debugging

--- server/lib/assembler.js
+++ server/lib/assembler.js
@@ -1,6 +1,7 @@
 var Async = require('async'),
     Frontmatter = require('front-matter'),
     Fs = require('fs'),
+    _ =require('lodash'),
     Hoek = require('hoek'),
     Path = require('path'),
     internals = {
@@ -31,30 +32,67 @@ function assemble(mainTemplate, callback) {
         return callback('The file config.json is missing in the root of the theme.');
     }
 
-    resolvePartials(mainTemplate, function(err, mainContent) {
-        var frontmatter = Frontmatter(mainContent),
-            config = frontmatter.attributes,
-            defaultConfig = Fs.readFileSync('config.json', {encoding: 'utf-8'});
+    Async.parallel([
+            //Load up all options templates, this is fine since it's devmode only
+            function(cb) {
+                Async.map([
+                        'components/products/options/date',
+                        'components/products/options/input-checkbox',
+                        'components/products/options/input-file',
+                        'components/products/options/input-numbers',
+                        'components/products/options/input-text',
+                        'components/products/options/set-radio',
+                        'components/products/options/set-rectangle',
+                        'components/products/options/set-select',
+                        'components/products/options/swatch',
+                        'components/products/options/textarea',
 
-        if (templatesMissing.length > 0) {
-            return callback('The following template(s) are/is missing: \n' + templatesMissing.join('\n'))
+                        'components/products/customizations/text',
+                        'components/products/customizations/checkbox',
+                        'components/products/customizations/file',
+                        'components/products/customizations/select',
+                        'components/products/customizations/textarea'
+                    ],
+                    function(val, doneCallback) {
+                        resolvePartials(val, doneCallback);
+                    },
+                    function (err, results) {
+                        cb(err);
+                    }
+                )
+            },
+            function(cb) {
+                resolvePartials(mainTemplate, function (err, mainContent) {
+                    var frontmatter = Frontmatter(mainContent),
+                        config = frontmatter.attributes,
+                        defaultConfig = Fs.readFileSync('config.json', {encoding: 'utf-8'});
+
+                    if (templatesMissing.length > 0) {
+                        return callback( 'The following template(s) are/is missing: \n' + templatesMissing.join('\n'))
+                    }
+
+                    try {
+                        defaultConfig = JSON.parse(defaultConfig);
+                    } catch (e) {
+                        return callback(e);
+                    }
+
+                    Hoek.merge(config, defaultConfig);
+                    // Replace main template content with just the body of frontmatter.
+                    templates[mainTemplate] = frontmatter.body;
+
+                    cb(null, config);
+                });
+            }
+        ],
+        function (err, results) {
+            var config = results[1];
+            callback(null, {
+                config: config,
+                templates: templates
+            });
         }
-
-        try {
-            defaultConfig = JSON.parse(defaultConfig);
-        } catch (e) {
-            return callback(e);
-        }
-
-        Hoek.merge(config, defaultConfig);
-        // Replace main template content with just the body of frontmatter.
-        templates[mainTemplate] = frontmatter.body;
-
-        callback(null, {
-            config: config,
-            templates: templates
-        });
-    });
+    )
 
     function resolvePartials(templateFile, callback) {
         callback = Hoek.nextTick(callback);
