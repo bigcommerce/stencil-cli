d3a47d3c0882f2e6a84732c531185690e805218d
Chris Hegre <christopher.hegre@bigcommerce.com>
2015-05-14 13:34:26 -0700

    BIG-18031: Localization via json files

---
+++ server/lib/localizer.js
@@ -0,0 +1,27 @@
+var _ = require('lodash'),
+    Fs = require('fs'),
+    Hoek = require('hoek'),
+    MessageFormat = require('messageformat'),
+    parser = require('accept-language-parser');
+
+
+module.exports.getPreferredTranslation = function(acceptLanguageHeader, translations) {
+    // default the preferred translation
+    var preferredTranslation = translations['en'],
+        preferredLang = parser.parse(acceptLanguageHeader);
+    // march down the preferred languages and use the first translatable locale
+    _.each(preferredLang, function(acceptedLang) {
+        var suitableLang = acceptedLang.code;
+
+        if (_.isString(acceptedLang.region)) {
+            suitableLang += '-' + acceptedLang.region;
+        }
+
+        if (translations[suitableLang]) {
+            preferredTranslation = translations[suitableLang];
+            return false;
+        }
+    });
+
+    return preferredTranslation;
+};
