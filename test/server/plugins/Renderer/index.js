e639a350a2ea600f574cac9e0046b25b3342b154
Mario Campa <mario.campa@bigcommerce.com>
2015-11-05 11:15:01 -0800

    Stencil-cli refactor. Theme registry API endpoints (squashed to solve conflicts)

--- test/server/plugins/Renderer/index.js
+++ test/server/plugins/Renderer/index.js
@@ -1,132 +1,394 @@
-'use strict';
-
-var _ = require('lodash'),
+var Assembler = require('../../server/lib/assembler'),
+    Boom = require('boom'),
     Code = require('code'),
     Hapi = require('hapi'),
+    FetchData = require('../../server/lib/fetchData'),
     Lab = require('lab'),
-    Path = require('path'),
-    sinon = require('sinon'),
-    Wreck = require('wreck'),
-    StencilCLI = require('../../../../server'),
+    Paper = require('stencil-paper'),
+    Sinon = require('sinon'),
     lab = exports.lab = Lab.script(),
     expect = Code.expect,
-    it = lab.it;
+    it = lab.it,
+    describe = lab.describe,
+    internals = {
+        paths: {
+            pluginsPath: './../../server/plugins'
+        }
+    };
 
-lab.describe('Renderer Plugin', function () {
-    var options = {
-            dotStencilFile: {
-                storeUrl: "https://store-abc123.mybigcommerce.com",
-                normalStoreUrl: "http://s123456789.mybigcommerce.com",
-                port: 4000,
-                apiKey: "abc123",
-                useCache: false
+describe('Renderer', function () {
+    var server = new Hapi.Server(),
+        replyResponseMock = {
+            code: function() {},
+            header: function() {},
+            type: function() {}
+        },
+        requestMock = {
+            app: {
+                storeUrl: 'https://mystore.com'
+            },
+            headers: {
+                host: 'localhost:3000'
             }
         },
-        server,
-        wreckRequestStub,
-        wreckReadStub;
+        replyMock = function() {
+            return replyResponseMock;
+        },
+        replySpy,
+        replyRedirectSpy,
+        replyResponseCodeSpy,
+        replyResponseHeaderSpy,
+        replyResponseTypeSpy;
 
-    lab.before(function (done) {
-        StencilCLI(options, function(err , srv) {
-           server = srv;
+    replyMock.redirect = function() {
+        return replyResponseMock;
+    };
 
-            // Don't log errors during the test
-            server.ext('onPostHandler', function (request, reply) {
-                if (request.response.isBoom) {
-                    return reply().code(500);
+    replySpy = Sinon.spy(replyMock);
+    replyRedirectSpy = Sinon.spy(replyMock, 'redirect');
+    replyResponseCodeSpy = Sinon.spy(replyResponseMock, 'code');
+    replyResponseHeaderSpy = Sinon.spy(replyResponseMock, 'header');
+    replyResponseTypeSpy = Sinon.spy(replyResponseMock, 'type');
+
+    server.connection({port: 3000});
+
+    lab.before(function(done) {
+        server.register([{
+            register: require(internals.paths.pluginsPath + '/Renderer')
+        }], function (err) {
+            expect(err).to.equal(undefined);
+            done();
+        });
+    });
+
+    it('should reply with Boom error if fetchData returns error', {only: true}, function(done) {
+        var error = new Error('No good!'),
+            fetchDataStub = Sinon.stub(FetchData, 'fetch');
+
+        fetchDataStub.callsArgWith(2, error);
+        server.plugins.Renderer.implementation(requestMock, replySpy);
+
+        expect(replySpy.calledWith(Boom.wrap(error))).to.equal(true);
+
+        fetchDataStub.restore();
+
+        done();
+    });
+
+    describe('with redirects', function() {
+        it('should set correct redirect headers if fetchData response headers has location set', function(done) {
+            var fetchDataStub = Sinon.stub(FetchData, 'fetch');
+
+            fetchDataStub.callsArgWith(2, null, {
+                statusCode: 301,
+                headers: {
+                    location: 'http://localhost:3000/redirect-me'
                 }
-
-                reply.continue();
             });
 
+            server.plugins.Renderer.implementation(requestMock, replyMock);
+
+            expect(replyResponseCodeSpy.calledWith(301)).to.equal(true);
+            expect(replyRedirectSpy.calledWith('http://localhost:3000/redirect-me')).to.equal(true);
+
+            fetchDataStub.restore();
+            done();
+        });
+
+        it('should set cookies if present in the header', function(done) {
+            var cookies = ['hey', 'there'],
+                fetchDataStub = Sinon.stub(FetchData, 'fetch');
+
+            fetchDataStub.callsArgWith(2, null, {
+                statusCode: 301,
+                headers: {
+                    location: 'http://localhost:3000/redirect-me',
+                    'set-cookie': cookies
+                }
+            });
+
+            server.plugins.Renderer.implementation(requestMock, replyMock);
+
+            expect(replyResponseHeaderSpy.calledWith('set-cookie', cookies)).to.equal(true);
+
+            fetchDataStub.restore();
             done();
         });
     });
 
-    lab.beforeEach(function (done) {
-        wreckRequestStub = sinon.stub(Wreck, 'request');
-        wreckReadStub = sinon.stub(Wreck, 'read');
+    describe('with rawData', function() {
+        it('should reply with rawData if returned from fetchData', function(done) {
+            var contentBuffer = new Buffer('Hi There!'),
+                fetchDataStub = Sinon.stub(FetchData, 'fetch');
 
-        done();
-    });
+            fetchDataStub.callsArgWith(2, null, {
+                statusCode: 200,
+                headers: {
+                    'content-type': 'text/plain'
+                },
+                rawData: contentBuffer
+            });
 
-    lab.afterEach(function (done) {
-        wreckRequestStub.restore();
-        wreckReadStub.restore();
+            server.plugins.Renderer.implementation(requestMock, replySpy);
 
-        done();
-    });
+            expect(replyResponseCodeSpy.calledWith(200)).to.equal(true);
+            expect(replyResponseTypeSpy.calledWith('text/plain')).to.equal(true);
+            expect(replySpy.calledWith(contentBuffer)).to.equal(true);
 
-    it('should handle fatal errors in the BCApp request', function (done) {
-        var options = {
-            method: "GET",
-            url: "/test"
-        };
+            fetchDataStub.restore();
+            done();
+        });
 
-        wreckRequestStub.callsArgWith(3, new Error('failure'));
+        it('should set cookies if present in the header', function(done) {
+            var cookies = ['hey', 'there'],
+                contentBuffer = new Buffer('Hi There!'),
+                fetchDataStub = Sinon.stub(FetchData, 'fetch');
 
-        server.inject(options, function (response) {
-            expect(response.statusCode).to.equal(500);
+            fetchDataStub.callsArgWith(2, null, {
+                statusCode: 200,
+                headers: {
+                    'set-cookie': cookies
+                },
+                rawData: contentBuffer
+            });
 
+            server.plugins.Renderer.implementation(requestMock, replySpy);
+
+            expect(replyResponseHeaderSpy.calledWith('set-cookie', cookies)).to.equal(true);
+
+            fetchDataStub.restore();
             done();
         });
     });
 
+    describe('with regular request', function() {
+        it('should reply with Boom error if Assembler returns an error', function(done) {
+            var error = new Error('No Good!'),
+                templateFile = 'test.html',
+                fetchDataStub = Sinon.stub(FetchData, 'fetch'),
+                assemblerStub = Sinon.stub(Assembler, 'assemble');
 
-    it('should handle responses of a 500 in the BCApp request', function (done) {
-        var options = {
-            method: "GET",
-            url: "/"
-        };
+            fetchDataStub.callsArgWith(2, null, {
+                statusCode: 200,
+                headers: {
+                    'content-type': 'text/plain'
+                },
+                template_file: templateFile
+            });
 
-        wreckRequestStub.callsArgWith(3, null, {
-            statusCode: 500
+            assemblerStub.callsArgWith(1, error);
+
+            server.plugins.Renderer.implementation(requestMock, replySpy);
+
+            expect(replySpy.calledWith(Boom.wrap(error))).to.equal(true);
+
+            fetchDataStub.restore();
+            assemblerStub.restore();
+            done();
         });
 
-        server.inject(options, function(response) {
-            expect(response.statusCode).to.equal(500);
+        it('should reply with Boom error if the second FetchData returns an error', function(done) {
+            var error = new Error('No Good!'),
+                templateFile = 'test.html',
+                fetchDataStub = Sinon.stub(FetchData, 'fetch'),
+                assemblerStub = Sinon.stub(Assembler, 'assemble'),
+                paperStub = Sinon.stub(Paper, 'compileSync');
+
+            fetchDataStub.onFirstCall().callsArgWith(2, null, {
+                statusCode: 200,
+                headers: {
+                    'content-type': 'text/plain'
+                },
+                template_file: templateFile,
+                context: {
+                    settings: {
+                        base_url: 'http://mystore.com',
+                        secure_base_url: 'https://mystore.com'
+                    }
+                }
+            });
+
+            paperStub.returns('');
+
+            fetchDataStub.onSecondCall().callsArgWith(2, error);
+
+            assemblerStub.callsArgWith(1, null, {
+                config: {},
+                templates: {}
+            });
+
+            requestMock.query = {
+                debug: false
+            };
+
+            server.plugins.Renderer.implementation(requestMock, replySpy);
+
+            expect(replySpy.calledWith(Boom.wrap(error))).to.equal(true);
+
+            fetchDataStub.restore();
+            assemblerStub.restore();
+            paperStub.restore();
 
             done();
         });
-    });
 
-    it('should handle redirects in the BCApp request', function (done) {
-        var options = {
-            method: "GET",
-            url: "/"
-        };
+        it('should reply with properly decorated content without debug bar', function(done) {
+            var context = {
+                    foo: 'bar',
+                    settings: {
+                        base_url: 'http://mystore.com',
+                        secure_base_url: 'https://mystore.com'
+                    }
+                },
+                templateFile = 'test.html',
+                renderedContent = '<body>' +
+                    '<a href="' + context.settings.base_url + '/foo">Base Url</a>' +
+                    '<a href="' + context.settings.secure_base_url + '/secure-foo">Secure Base Url</a>' +
+                    '</body>',
+                decoratedContent = '<body>' +
+                    '<a href="/foo">Base Url</a>' +
+                    '<a href="/secure-foo">Secure Base Url</a>' +
+                    '</body>',
+                templates = {
+                    foo: 'bar.html'
+                },
+                fetchDataStub = Sinon.stub(FetchData, 'fetch'),
+                assemblerStub = Sinon.stub(Assembler, 'assemble'),
+                paperStub = Sinon.stub(Paper, 'compileSync');
 
-        wreckRequestStub.callsArgWith(3, null, {
-            statusCode: 301,
-            headers: {
-                location: 'http://www.example.com/'
-            }
-        });
+            requestMock.query = {
+                debug: false
+            };
 
-        server.inject(options, function(response) {
-            expect(response.statusCode).to.equal(301);
-            expect(response.headers.location).to.equal('http://www.example.com/');
+            fetchDataStub.callsArgWith(2, null, {
+                statusCode: 200,
+                headers: {
+                    'content-type': 'text/plain'
+                },
+                template_file: templateFile,
+                context: context
+            });
+
+            assemblerStub.callsArgWith(1, null, {
+                config: {},
+                templates: templates
+            });
+
+            paperStub.returns(renderedContent);
+
+            server.plugins.Renderer.implementation(requestMock, replySpy);
+
+            expect(replyResponseCodeSpy.calledWith(200)).to.equal(true);
+            expect(paperStub.calledWith(templateFile, templates, context));
+            expect(replySpy.calledWith(decoratedContent)).to.equal(true);
+
+            fetchDataStub.restore();
+            assemblerStub.restore();
+            paperStub.restore();
+
+            delete requestMock.query;
 
             done();
         });
-    });
 
-    it('should handle redirects in the BCApp request', function (done) {
-        var options = {
-            method: "GET",
-            url: "/"
-        };
+        it('should reply with debug bar if debug is set to "bar"', function(done) {
+            var context = {
+                    foo: 'bar',
+                    settings: {
+                        base_url: 'http://mystore.com',
+                        secure_base_url: 'https://mystore.com'
+                    }
+                },
+                templateFile = 'test.html',
+                renderedContent = '<body></body>',
+                decoratedContent =
+                    '<body><pre><p><b>Context:</b></p>' + JSON.stringify(context, null, 2) + '</pre>\n</body>',
+                templates = {
+                    foo: 'bar.html'
+                },
+                fetchDataStub = Sinon.stub(FetchData, 'fetch'),
+                assemblerStub = Sinon.stub(Assembler, 'assemble'),
+                paperStub = Sinon.stub(Paper, 'compileSync');
 
-        wreckRequestStub.callsArgWith(3, null, {
-            statusCode: 301,
-            headers: {
-                location: 'http://www.example.com/'
-            }
+            requestMock.query = {
+                debug: 'bar'
+            };
+
+            fetchDataStub.callsArgWith(2, null, {
+                statusCode: 200,
+                headers: {
+                    'content-type': 'text/plain'
+                },
+                template_file: templateFile,
+                context: context
+            });
+
+            assemblerStub.callsArgWith(1, null, {
+                config: {},
+                templates: templates
+            });
+
+            paperStub.returns(renderedContent);
+
+            server.plugins.Renderer.implementation(requestMock, replySpy);
+
+            expect(replyResponseCodeSpy.calledWith(200)).to.equal(true);
+            expect(paperStub.calledWith(templateFile, templates, context));
+            expect(replySpy.calledWith(decoratedContent)).to.equal(true);
+
+            fetchDataStub.restore();
+            assemblerStub.restore();
+            paperStub.restore();
+
+            delete requestMock.query;
+
+            done();
         });
 
-        server.inject(options, function(response) {
-            expect(response.statusCode).to.equal(301);
-            expect(response.headers.location).to.equal('http://www.example.com/');
+        it('should set cookies if set-cookies is in the headers', function(done) {
+            var context = {
+                    foo: 'bar',
+                    settings: {
+                        base_url: 'http://mystore.com',
+                        secure_base_url: 'https://mystore.com'
+                    }
+                },
+                cookies = ['hey', 'there'],
+                templateFile = 'test.html',
+                fetchDataStub = Sinon.stub(FetchData, 'fetch'),
+                assemblerStub = Sinon.stub(Assembler, 'assemble'),
+                paperStub = Sinon.stub(Paper, 'compileSync');
+
+            requestMock.query = {
+                debug: 'bar'
+            };
+
+            fetchDataStub.callsArgWith(2, null, {
+                statusCode: 200,
+                headers: {
+                    'content-type': 'text/plain',
+                    'set-cookie': cookies
+                },
+                template_file: templateFile,
+                context: context
+            });
+
+            assemblerStub.callsArgWith(1, null, {
+                config: {},
+                templates: {}
+            });
+
+            paperStub.returns('');
+
+            server.plugins.Renderer.implementation(requestMock, replySpy);
+
+            expect(replyResponseHeaderSpy.calledWith('set-cookie', cookies)).to.equal(true);
+
+            fetchDataStub.restore();
+            assemblerStub.restore();
+            paperStub.restore();
+
+            delete requestMock.query;
 
             done();
         });
