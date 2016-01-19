dd25887bdd8aba894cc3c1662fdc2391615e8687
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2016-01-19 15:19:39 -0800

    BIG-24290: Updated ngIdle and stencil-editor

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,4 @@ import 'bigcommerce-labs/ng-common/dist/js/ng-common.min';
 import 'jmdobry/angular-cache/dist/angular-cache.min';
 import 'bigcommerce-labs/bcapp-pattern-lab/dist/js/bcapp-pattern-lab';
 import 'angular-formly';
+import 'ng-idle';
