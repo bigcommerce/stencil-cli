6b3ad02cd19e7d1d93713de9aade80c81130e031
bc-zoharmuzafi <zohar.muzafi@bigcommerce.com>
2018-11-28 14:37:49 -0800

    added release script include auto generate of changelog

---
+++ tasks/changelog/command-executor.js
@@ -0,0 +1,49 @@
+const toSpawnArgs = require('object-to-spawn-args');
+const which = require('npm-which')(__dirname);
+
+/**
+ * @param {Object} childProcess
+ * @constructor
+ */
+function CommandExecutor(childProcess) {
+    /**
+     * @param {string} executable
+     * @param {string[]} argv
+     * @param {Object} options
+     * @param {function(error: Error?): void} done
+     * @return {void}
+     */
+    function executeCommand(executable, argv, options, done) {
+        const command = createCommand(executable, argv, options);
+        
+        childProcess.spawn(command.executable, command.args, command.options)
+            .on('close', (code) => {
+                done(code ? new Error(`Exit code: ${code}`) : undefined);
+            });
+    }
+
+    /**
+     * @private
+     * @param {string} executable
+     * @param {string[]} [argv=[]]
+     * @param {Object} [options={}]
+     * @return {Object}
+     */
+    function createCommand(executable, argv, options) {
+        argv = argv ? argv : [];
+        options = options ? options : {};
+        const executablePath = which.sync(executable);
+        const spawnArgs = toSpawnArgs(options);
+        const spawnOptions = { stdio: 'inherit' };
+
+        return {
+            executable: executablePath,
+            args: spawnArgs.concat(argv),
+            options: spawnOptions,
+        };
+    }
+
+    this.executeCommand = executeCommand;
+}
+
+module.exports = CommandExecutor;
---
+++ tasks/changelog/command-executor.spec.js
@@ -0,0 +1,65 @@
+const CommandExecutor = require('./command-executor');
+const Code = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const sinon = require('sinon');
+const expect = Code.expect;
+const it = lab.it;
+
+describe('CommandExecutor', () => {
+    let commandExecutor;
+    let childProcess;
+    let spawnSpy;
+    let processMock;
+    let spawn;
+    let doneCallBack;
+
+    it('spawns a new process to run the given command', done => {
+        childProcess = require('child_process');
+        spawnSpy = sinon.spy(childProcess, "spawn");
+        commandExecutor = new CommandExecutor(childProcess);
+        
+        commandExecutor.executeCommand('jest', ['xyz.js'], { config: 'config.js' }, () => {
+            expect(spawnSpy.calledWith(
+                require('npm-which')(__dirname).sync('jest'),
+                ['--config', 'config.js', 'xyz.js'],
+                { stdio: 'inherit' }
+            )).to.equal(true);
+
+            done();
+        });
+    });
+
+    it('returns undefined if the process exits with a successful exit code', done => {
+        processMock = {
+            on: function(event, callback) { return callback(0) },
+        };
+
+        spawn = function() { return processMock };
+
+        doneCallBack = sinon.fake();
+        commandExecutor = new CommandExecutor({ spawn });
+        commandExecutor.executeCommand('jest', ['xyz.js'], { config: 'config.js' }, doneCallBack);
+
+        expect(doneCallBack.calledWith(undefined)).to.equal(true);
+
+        done();
+    });
+
+    it('returns error if the process exits with an unsuccessful exit code', done => {
+        processMock = {
+            on: function(event, callback) { return callback(1) },
+        };
+
+        spawn = function() { return processMock };
+        
+        doneCallBack = sinon.fake();
+        commandExecutor = new CommandExecutor({ spawn });
+        commandExecutor.executeCommand('jest', ['xyz.js'], { config: 'config.js' }, doneCallBack);
+        
+        expect(doneCallBack.calledWith(undefined)).to.equal(false);
+
+        done();
+    });
+});
