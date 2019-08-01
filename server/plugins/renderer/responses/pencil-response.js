eb5ef785684f27d60eb8c79952fbacc911c8d426
William Kwon <william.kwon@bigcommerce.com>
2019-08-01 15:44:38 -0700

    feat: strf-7175 bump paper to 3.0.0-rc.19

--- server/plugins/renderer/responses/pencil-response.js
+++ server/plugins/renderer/responses/pencil-response.js
@@ -4,12 +4,7 @@ const internals = {};
 
 module.exports = function (data, assembler) {
     this.respond = function (request, reply) {
-        var response,
-            output,
-            paper,
-            templatePath;
-
-        var paper = new Paper(data.context.settings, data.context.theme_settings, assembler);
+        const paper = new Paper(data.context.settings, data.context.theme_settings, assembler, "handlebars-v3");
 
         // Set the environment to dev
         data.context.in_development = true;
@@ -22,21 +17,21 @@ module.exports = function (data, assembler) {
             paper.addDecorator(decorator);
         })
 
-        templatePath = internals.getTemplatePath(request, data);
+        const templatePath = internals.getTemplatePath(request, data);
 
-        paper.loadTheme(templatePath, data.acceptLanguage, function () {
+        paper.loadTheme(templatePath, data.acceptLanguage).then(() => {
             if (request.query.debug === 'context') {
                 return reply(data.context);
             }
 
-            output = paper.renderTheme(templatePath, data);
-            response = reply(output);
+            const output = paper.renderTheme(templatePath, data);
+            const response = reply(output);
             response.code(data.statusCode);
 
             if (data.headers['set-cookie']) {
                 response.header('set-cookie', data.headers['set-cookie']);
             }
-        });
+        }).catch(err => console.error(err.message.red));
     };
 };
 
