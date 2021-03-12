7582e7c85b4b6c858a25670a13e7981611e2ca03
MaxGenash <max.henash@bigcommerce.com>
2021-03-12 20:39:36 +0200

    feat: strf-7651 - update push command to take into account multiple channels

--- bin/stencil-push.js
+++ bin/stencil-push.js
@@ -9,11 +9,16 @@ const { printCliResultErrorAndExit } = require('../lib/cliCommon');
 
 program
     .version(PACKAGE_INFO.version)
-    .option('--host [hostname]', 'specify the api host', API_HOST)
+    .option('--host [hostname]', 'specify the api host')
     .option('-f, --file [filename]', 'specify the filename of the bundle to upload')
     .option('-s, --save [filename]', 'specify the filename to save the bundle as')
     .option('-a, --activate [variationname]', 'specify the variation of the theme to activate')
     .option('-d, --delete', 'delete oldest private theme if upload limit reached')
+    .option(
+        '-c, --channel_id [channelId]',
+        'specify the channel ID of the storefront to push the theme to',
+        parseInt,
+    )
     .parse(process.argv);
 
 checkNodeVersion();
@@ -21,6 +26,7 @@ checkNodeVersion();
 const cliOptions = program.opts();
 const options = {
     apiHost: cliOptions.host || API_HOST,
+    channelId: cliOptions.channel_id,
     bundleZipPath: cliOptions.file,
     activate: cliOptions.activate,
     saveBundleName: cliOptions.save,
