cb6d72e853b4e30456b7201db0ce923a33697f81
Jairo Panduro <jairo.panduro@bigcommerce.com>
2021-08-06 19:21:12 +0300

    fix: strf-9257 Added check if theme version exists

--- bin/stencil-release.js
+++ bin/stencil-release.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 require('colors');
-const release = require('../lib/release/release');
+const StencilRelease = require('../lib/release/release');
 const { PACKAGE_INFO } = require('../constants');
 const program = require('../lib/commander');
 const { checkNodeVersion } = require('../lib/cliCommon');
+const { printCliResultErrorAndExit } = require('../lib/cliCommon');
 
 program.version(PACKAGE_INFO.version).parse(process.argv);
 
 checkNodeVersion();
-
-release();
+new StencilRelease().run().catch(printCliResultErrorAndExit);
