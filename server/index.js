c08de59134e8ed38185a3dcb26e4318b4d3432cc
bc-max <max.moroz@bigcommerce.com>
2022-09-16 19:58:40 +0300

    feat(storefront): strf-9832 Inconsistent behavior for FM in custom templates:
    - renderer.module.js - sends additional header to bcapp (customLayouts);
    - renderer.module.spec.js - tests amended;
    - index.js - custom layouts added;

--- server/index.js
+++ server/index.js
@@ -33,6 +33,8 @@ function buildManifest(srcManifest, options) {
     pluginsByName['./plugins/renderer/renderer.module'].storeUrl = storeUrl;
     pluginsByName['./plugins/renderer/renderer.module'].storeSettingsLocale =
         options.storeSettingsLocale;
+    pluginsByName['./plugins/renderer/renderer.module'].customLayouts =
+        options.dotStencilFile.customLayouts;
     pluginsByName['./plugins/theme-assets/theme-assets.module'].themePath = options.themePath;
 
     resManifest.register.plugins = _.reduce(
