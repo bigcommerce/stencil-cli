4f9024e2d3b72a8cf7cf428c67fd37e29342d1e5
Parth Shah <parth.shah@bigcommerce.com>
2026-07-22 13:31:31 -0400

    fix: TRAC-514 - accept ticket-first commit format in release flow and use PAT for release PRs

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,10 @@
+const { parserOpts, types } = require('./commit-format.cjs');
+
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    parserPreset: { parserOpts },
     rules: {
         'subject-case': [0, 'always', 'sentence-case'],
+        'type-enum': [2, 'always', types],
     },
 };
